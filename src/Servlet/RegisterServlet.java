1ac5ba118e3bc6b56943cde07377c067b2a25575
joen <15065379+Joen408@user.noreply.gitee.com>
2024-12-01 18:55:10 +0800

    Init_Structure

---
+++ src/Servlet/RegisterServlet.java
@@ -0,0 +1,23 @@
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
