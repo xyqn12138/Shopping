5e5a636adbe13a4b5cff41a8730168565d673eb9
joen <15065379+Joen408@user.noreply.gitee.com>
2024-12-09 09:43:06 +0800

    login

--- src/domain/RegisterLogin/DBUtil.java
+++ src/domain/RegisterLogin/DBUtil.java
@@ -23,6 +23,7 @@ public class DBUtil {
         return true;
     }
     public boolean verifyUser(String username, String password) {
+        if(users.isEmpty())return false;
         UserBean user = users.get(username);
         if(!users.containsKey(user.getUsername()))return false;
         return password.equals(user.getPassword());
